4edd11be7a1682c8fd794e0ea257ba47922de8d8
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-02-13 17:00:38 +0900

    ユーザー管理機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # 入力フォームからアカウント名情報をDBに保存するために追加
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
