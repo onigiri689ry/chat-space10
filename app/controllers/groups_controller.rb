7523429560886bb8229ab783a6549ec119705365
onigiri689ry <59249071+onigiri689ry@users.noreply.github.com>
2020-02-14 18:09:30 +0900

    グループの新規登録・編集機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,7 @@
 class GroupsController < ApplicationController
 
+  before_action :set_group, only: [:edit, :update]
+
   def index
   end
 
@@ -35,4 +37,8 @@ class GroupsController < ApplicationController
     params.require(:group).permit(:name, user_ids: [])
   end
   
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
 end
